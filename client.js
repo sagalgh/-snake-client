705eeb0963ca8b5d8024e8bfca5f178e2b271cf7
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-03 18:01:06 -0400

    added initials

--- client.js
+++ client.js
@@ -1,15 +1,17 @@
 const net = require("net");;
+const name = "TLK"
 const connect = function () {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: '50541',
   });
+ 
   conn.on("connect", () => {
-    console.log("connection established")
+    console.log("Successfully connected to game server!")
+    conn.write(`Name: ${name}`);
   });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
 
+  conn.setEncoding("utf8");
   return conn;
 };
 
