ba7d136ec4aad5640be9ed8cc176613dff892bcf
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-03 19:27:35 -0400

    added functions to help establish UI where players can send keyboard commands

--- play.js
+++ play.js
@@ -1,8 +1,18 @@
 const {connect} = require("./client");
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
 
-// establishes a connection with the game server
-
-
-
+setupInput()
 console.log("Connecting ...");
 connect();
